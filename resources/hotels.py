47875bb112a16cbfcdfb5ff80150b49cef63346f
jenny-xing <5709881+jenny-xing@users.noreply.github.com>
2020-06-12 15:13:47 -0400

    Revert hotels.py

--- resources/hotels.py
+++ resources/hotels.py
@@ -1,13 +1,16 @@
+
 from datetime import datetime
 from flask import abort
 from flask_restful import Resource, reqparse, fields, marshal
-from models import db, Hotels
 
 # TODO: create a postgres database and store these there
 hotels = [
     {
     'id': 1,
-    'name': 'The Asteroid',
+    'hotel_name': 'The Asteroid',
+    'total_double_rooms': 10,
+    'total_queen_rooms': 10,
+    'total_king_rooms': 10,
     'created_date': u'2020-05-01T14:09:13.702495',
     'last_updated_date': u'2020-05-01T14:09:13.702495'
     }
@@ -15,7 +18,7 @@ hotels = [
 
 hotel_fields = {
     'id': fields.Integer,
-    'name': fields.String,
+    'hotel_name': fields.String,
     'total_double_rooms': fields.Integer,
     'total_queen_rooms': fields.Integer,
     'total_king_rooms': fields.Integer,
@@ -26,7 +29,7 @@ hotel_fields = {
 
 # Parser for HotelList and Hotel resources
 reqparse = reqparse.RequestParser()
-reqparse.add_argument('name', type = str, required = True,
+reqparse.add_argument('hotel_name', type = str, required = True,
             help = "No hotel name provided.", location = 'json')
 reqparse.add_argument('total_double_rooms', type = int, required = True,
     help = "Total number of double rooms in the hotel not provided.", location = 'json')
@@ -48,18 +51,15 @@ class HotelList(Resource):
         """Add a new reservation to the reservations list."""
         args = self.reqparse.parse_args()
         hotel = {
-                 #'id': hotels[-1]['id'] + 1 if len(hotels) > 0 else 1,
-                 'name': args['name'],
-                 #'total_double_rooms': args['total_double_rooms'],
-                 #'total_queen_rooms': args['total_queen_rooms'],
-                 #'total_king_rooms': args['total_king_rooms'],
+                 'id': hotels[-1]['id'] + 1 if len(hotels) > 0 else 1,
+                 'hotel_name': args['hotel_name'],
+                 'total_double_rooms': args['total_double_rooms'],
+                 'total_queen_rooms': args['total_queen_rooms'],
+                 'total_king_rooms': args['total_king_rooms'],
                  'created_date': datetime.now().isoformat(),
-                 #'last_updated_date': datetime.now().isoformat()
+                 'last_updated_date': datetime.now().isoformat()
                  }    
-        #hotels.append(hotel)
-        hotel = Hotels(**hotel)
-        db.session.add(hotel)
-        db.session.commit()
+        hotels.append(hotel)
         return {'hotels': marshal(hotel, hotel_fields)}, 201
 
 class Hotel(Resource):
